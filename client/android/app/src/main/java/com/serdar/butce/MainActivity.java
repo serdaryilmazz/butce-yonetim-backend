c3cefb3970a853496e7a1fa564256d0b9e93d95c
serdaryilmazz <srd4646@outlook.com>
2026-04-16 15:28:34 +0300

    Feature: Full mobile app (Capacitor & Android) setup added

---
+++ client/android/app/src/main/java/com/serdar/butce/MainActivity.java
@@ -0,0 +1,5 @@
+package com.serdar.butce;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.serdar.butce',
+  appName: 'ButceYonetim',
+  webDir: 'dist/client/browser'
+};
+
+export default config;
